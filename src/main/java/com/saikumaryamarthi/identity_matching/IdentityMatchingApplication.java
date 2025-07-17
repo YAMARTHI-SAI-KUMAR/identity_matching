07e6311faa9d01ead63cdc04ec2e0d7eabc7112c
Sai Kumar Yamarthi <saikumar.y786@gmail.com>
2025-07-18 02:54:42 +0530

    Initial commit

---
+++ src/main/java/com/saikumaryamarthi/identity_matching/IdentityMatchingApplication.java
@@ -0,0 +1,13 @@
+package com.saikumaryamarthi.identity_matching;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentityMatchingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentityMatchingApplication.class, args);
+	}
+
+}
